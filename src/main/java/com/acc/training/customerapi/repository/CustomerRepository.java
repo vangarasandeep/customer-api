c6c69a37abcb302e2206380baaca1a118840df33
alexbt-zz <alexbt@gmx.us>
2022-01-11 19:08:53 -0600

    Implementing mongodb connections

--- src/main/java/com/acc/training/customerapi/repository/CustomerRepository.java
+++ src/main/java/com/acc/training/customerapi/repository/CustomerRepository.java
@@ -1,42 +1,10 @@
 package com.acc.training.customerapi.repository;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.acc.training.customerapi.domain.CustomerDomain;
 
-import org.springframework.stereotype.Repository;
-import com.acc.training.customerapi.model.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-@Repository
-public class CustomerRepository {
+public interface CustomerRepository extends MongoRepository<CustomerDomain, String> {
 
-    private static final Map<String, Customer> customerMap = new HashMap<>();
-
-    static {
-        initCustomerDb();
-    }
-
-    private static void initCustomerDb() {
-
-        Customer customer1 = new Customer();
-        customer1.setCustomerId("12345");
-        customer1.setCustomerName("Joe Doe");
-        customer1.customerAddress("1000 Main Street");
-
-        Customer customer2 = new Customer();
-        customer2.setCustomerId("12348");
-        customer2.setCustomerName("Jane Doe");
-        customer2.customerAddress("2000 Main Street");
-
-        customerMap.put(customer1.getCustomerId(), customer1);
-        customerMap.put(customer2.getCustomerId(), customer2);
-    }
-
-    public Customer getCustomer(String id) {
-        return customerMap.get(id);
-    }
-
-    public Customer saveCustomer(Customer customer) {
-        customerMap.put(customer.getCustomerId(), customer);
-        return customerMap.get(customer.getCustomerId());
-    }
+    CustomerDomain findByCustomerId(String customerId);
 }
