1e52fbc770edc70667ccc577509bf521c860675e
Swati Jain <swati.an.jain@accenture.com>
2022-01-11 20:05:14 -0500

    Commiting Customer API

--- src/main/java/com/acc/training/customerapi/Repository/CustomerRepository.java
+++ src/main/java/com/acc/training/customerapi/Repository/CustomerRepository.java
@@ -4,45 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.acc.training.customerapi.model.Customer;
+import com.acc.training.domain.CustomerDomain;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class CustomerRepository {
 
-    private static final Map<String, Customer> customerMap=new HashMap<>();
+public interface CustomerRepository extends MongoRepository<CustomerDomain, String>{
 
-    static
-    {
-        initCustomerDB();
-    }
-
-    private static void initCustomerDB() {
-        Customer cust1=new Customer();
-        cust1.setCustomerId("12345");
-        cust1.setCustomerName("Swati");
-        cust1.setCustomerAddress("Main Street");
-
-        customerMap.put(cust1.getCustomerId(),cust1);
-
-        Customer cust2=new Customer();
-        cust2.setCustomerId("12345");
-        cust2.setCustomerName("Swati");
-        cust2.setCustomerAddress("Main Street");
-
-        customerMap.put(cust1.getCustomerId(), cust2);
-
-    }
-    public Customer getCustomer(String id)
-    {
-        return customerMap.get(id);
-    
-    }
-
-    public Customer saveCustomer(Customer cust)
-    {
-        customerMap.put(cust.getCustomerId(),cust);
-        return customerMap.get(cust.getCustomerId());
-    }
+    public CustomerDomain findByCustomerId(String customerId);
     
 }
--- src/main/java/com/acc/training/customerapi/service/CustomerService.java
+++ src/main/java/com/acc/training/customerapi/service/CustomerService.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import com.acc.training.customerapi.Repository.CustomerRepository;
 import com.acc.training.customerapi.model.Customer;
+import com.acc.training.domain.CustomerDomain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,11 +16,29 @@ public class CustomerService {
     private CustomerRepository repository;
 
     public Customer createCustomer(@Valid Customer body) {
-        return repository.saveCustomer(body);
+       // return repository.saveCustomer(body);
+       CustomerDomain customerDomain=mapModelToDomain(body);
+       return mapDomainToModel(repository.save(customerDomain));
+
     }
 
     public Customer getCustomer(String id) {
-        return repository.getCustomer(id);
+        //return repository.getCustomer(id);
+        return mapDomainToModel(repository.findByCustomerId(id));
     }
+
+    private CustomerDomain mapModelToDomain(Customer body) {
+        CustomerDomain customerDomain = new CustomerDomain(); 
+        customerDomain.setCustomerName(body.getCustomerName());
+        customerDomain.setCustomerId(body.getCustomerId());
+        customerDomain.setCustomerAddress(body.getCustomerAddress());
+         return customerDomain;}
+
+         private Customer mapDomainToModel(CustomerDomain customerDomain) {
+            Customer customer = new Customer();
+            customer.setCustomerName(customerDomain.getCustomerName());
+            customer.setCustomerId(customerDomain.getCustomerId());
+            customer.setCustomerAddress(customerDomain.getCustomerAddress()); return customer;}
+
     
 }
