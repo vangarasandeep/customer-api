f7df7793e9c510e72a7c517a0b2bf851bd666575
Ashok Pola <ashok.pola@corteva.com>
2022-01-12 15:58:23 -0600

    MongoDB integration
    MongoDB integration

--- src/main/java/com/acc/training/customerapi/Repository/CustomerMongoRepository.java
+++ src/main/java/com/acc/training/customerapi/Repository/CustomerMongoRepository.java
@@ -1,14 +1,11 @@
 package com.acc.training.customerapi.Repository;
 
 
-
 import com.acc.training.customerapi.domain.CustomerDomain;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 
-
-public interface CustomerRepository extends MongoRepository<CustomerDomain, String> {
-
+public interface CustomerMongoRepository extends MongoRepository<CustomerDomain, String> {
     public CustomerDomain findByCustomerId(String customerId);
-    
+   
 }
--- src/main/java/com/acc/training/customerapi/service/CustomerService.java
+++ src/main/java/com/acc/training/customerapi/service/CustomerService.java
@@ -1,6 +1,9 @@
 package com.acc.training.customerapi.service;
 
+import javax.validation.Valid;
+
 import com.acc.training.customerapi.Repository.CustomerRepository;
+import com.acc.training.customerapi.domain.CustomerDomain;
 import com.acc.training.customerapi.model.Customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +13,31 @@ import org.springframework.stereotype.Service;
 public class CustomerService {
 
     @Autowired
+    private CustomerRepository repository;
 
-    private CustomerRepository custRepo;
-
-    public Customer createCustomer(Customer cust) {
-        return custRepo.saveCustomer(cust);
+    public Customer createCustomer(@Valid Customer body) {
+        CustomerDomain customerDomain = mapModelToDomain(body);
+        return mapDomainToModel(repository.save(customerDomain));
     }
 
     public Customer getCustomer(String id) {
-        return custRepo.getCustomer(id);
+        return mapDomainToModel(repository.findByCustomerId(id));
+    }
+
+    private CustomerDomain mapModelToDomain(Customer body) {
+        CustomerDomain customerDomain = new CustomerDomain();
+        customerDomain.setCustomerName(body.getCustomerName());
+        customerDomain.setCustomerId(body.getCustomerId());
+        customerDomain.setCustomerAddress(body.getCustomerAddress());
+        return customerDomain;
+    }
+
+    private Customer mapDomainToModel(CustomerDomain customerDomain) {
+        Customer customer = new Customer();
+        customer.setCustomerName(customerDomain.getCustomerName());
+        customer.setCustomerId(customerDomain.getCustomerId());
+        customer.setCustomerAddress(customerDomain.getCustomerAddress());
+        return customer;
     }
     
 }
